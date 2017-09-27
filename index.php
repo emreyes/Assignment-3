7b951b29ace214a19058e9f1075f6e751f179f48
Erin Reyes <erin.reyes01@utrgv.edu>
2017-09-27 17:33:28 -0500

    Changed index.html to index.php

---
+++ index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Attendance Tracker Application</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="styles.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>Need Help Tracking Attendance?</h1>
+        <p class="lead">
+          Are you tired of having to call out diffcult names and wasting class time?
+          <br>
+          By having students send in a code through their mobile devices, taking attendance has never been easier. Try out our software for 15 days by clicking down below!
+        </p>
+
+        <a class="btn btn-success" href="http://www.google.com" role="button">Start Free Trial Now</a>
+
+        <br><br><br>
+
+        <div class = "row">
+          <div class = "col-md-4">
+            <h3>Quick and Easy</h3>
+            <p>
+              With large audiences for classes, seminars, and even trainings, taking attendance can take away the time that could have been used towards teaching others during these sessions. In order to keep taking attendance from being time consuming, our software allows students to send a random-generated code given to their teachers to show they are present through our mobile application which is free of charge for the first 15 days. All that the students and teachers need to do is make an account which keeps track of their attendance for every class that uses this software. All tools and and features within the application are easy to find and to manage for all users, especially for universities who need to keep track of attendance of the students enrolled for classes.
+            </p>
+            <a class="btn btn-primary" href="#" role="button">View Details ></a>
+          </div>
+
+          <div class = "col-md-4">
+            <h3>For Students and Teachers</h3>
+            <p>
+              This software is primarily for students and teachers in a classroom setting, whether in elementary, high school, or colleges/universities. Teachers will use the application to check which students show up to class, and students will send in a code for attendance through their mobile devices given to then by their teachers. Universites can take advantage of this software in order to meet regulations for every time their accreditations need to be approved. 
+            </p>
+            <a class="btn btn-primary" href="#" role="button">View Details ></a>
+          </div>
+          
+          <div class = "col-md-4">
+            <h3>Some Features</h3>
+            <p>
+              The format of the software will be grid-like, listing all students that sign up for the class, for teachers. In order to view a student's attendance for the entire semester, the teacher just has to click on the student's name. For students, it will show what classes there are signed up for, and as the semester goes by, they will be shown their attendance record. Those classes that are dropped will be removed. When students send in the code, a checkmark will be added next to their name. There are several other features this application offers.
+            </p>
+            <a class="btn btn-primary" href="#" role="button">View Details ></a>
+          </div>
+        </div>
+
+        <br>
+    	<div class = "starter-row">
+	        <h2>About the Software</h2>
+	        <p>
+	          For simplification purposes, the software works primarily for taking attendance and is offered for the Android operating system but will be available in a website format. Every student and teacher in a class, seminar, or training has to make an account which only asks for their email, name, and password (Admin and User Login). Then they must verify their account before usage. Once that is done, a random code is generated and given to the teacher who then has the students enter in the code to mark that they are present. Since most students are tech savy, taking attendance should be done in a couple minutes which allows for more class time.
+	        </p>
+	    </div>
+
+		<div class = "starter-row">
+        	<br>
+        	<h2>More Affordable</h2>
+	        <p>
+	          This software will be offered to schools and other big organizations as a package service with an initial price of $600 for every four months. For a stand alone buyer, it will be priced at $5 per month.
+	        </p>
+	        <a class="btn btn-success" href="http://www.google.com" role="button">Sign Up Now!</a>
+	    </div>
+
+      </div>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="assets/js/vendor/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
